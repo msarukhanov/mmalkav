2ccd4ed5c77122a831f389050768153c8a750deb
Mark <msarukhanov@gmail.com>
2015-11-12 18:57:49 +0400

    refolder

--- README.md
+++
@@ -1,1 +0,0 @@
-# mmalkav
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static(__dirname + '/data'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('json spaces', 2);
+
+require('middleware/routes.js')(app, fs);
+
+var server = app.listen(901);
