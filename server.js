41e3c76eaaeead427d6e7e135eafac484edc19f9
Mark <msarukhanov@gmail.com>
2015-11-13 10:32:26 +0400

    heroku

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var app = express();
 
+var port = process.env.PORT || 901;
+
 app.use(express.static(__dirname + '/data'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,6 +21,6 @@ app.set('json spaces', 2);
 
 require('middleware/routes.js')(app, fs);
 
-var server = app.listen(process.env.PORT || 901, function() {
-    console.log("listening on " + process.env.PORT);
+var server = app.listen(port || 901, function() {
+    console.log("listening on " + port);
 });
