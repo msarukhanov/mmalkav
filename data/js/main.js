c20bbf0830e9767dbfbc82b94518ea489efe0371
Mark <msarukhanov@gmail.com>
2015-11-19 15:38:52 +0400

    users_online

--- data/js/main.js
+++ data/js/main.js
@@ -21,7 +21,7 @@ app.controller("mainCtrl", function($scope, $rootScope, $http) {
 
         var map = L.map('map', {
             crs: L.CRS.Simple,
-            maxZoom: 1,
+            maxZoom: 8,
             maxBounds: bounds
         }).setView([-1250, 700], 0);
         L.mapbox.accessToken = 'pk.eyJ1IjoibW1hbGthdiIsImEiOiJjaWg0aTRyaWswMHN1a3FseXQ2MjRrbnl0In0.TnjjiSL_H80Z0thxcF-rtw';
@@ -230,93 +230,347 @@ app.controller("mainCtrl", function($scope, $rootScope, $http) {
                             mapmodes.update('', '', '')
                         }
                     },
-                    click: function() {
+                    click: function(e) {
                         $('.mapmode').hide();
+                        console.log(e);
                     }
-                });
-
-
-                $scope.isLog = false;
-                $scope.login_error = false;
-                $scope.login = function(login, password) {
-                    $http({
-                        url: "/chat/signin",
-                        method: "POST",
-                        data: {
-                            login : login,
-                            password : password
-                        }
-                    }).success(function (data) {
-                        if(data._id) {
-                            $scope.login_error = false;
-                            var socket = io();
-                            var name = data.login;
-                            var messages = $("#messages");
-                            var message_txt = $("#message_text");
-                            $('.chat .nick').text(name);
-
-                            function msg(nick, message) {
-                                var m = '<div class="msg">' +
-                                    '<span class="user">' + safe(nick) + ':</span> '
-                                    + safe(message) +
-                                    '</div>';
-                                messages
-                                    .append(m)
-                                    .scrollTop(messages[0].scrollHeight);
-                            }
-
-                            function msg_system(message) {
-                                var m = '<div class="msg system">' + safe(message) + '</div>';
-                                messages
-                                    .append(m)
-                                    .scrollTop(messages[0].scrollHeight);
-                            }
-
-                            socket.on('connecting', function () {
-                                msg_system('Connecting...');
-                            });
-
-                            socket.on('connect', function (data) {
-                                msg_system('Successfully connected to map chat!');
-                                socket.emit("message", {message: name + ' connected!', name: 'Admin'});
-                            });
-
-                            socket.on('message', function (data) {
-                                msg(data.name, data.message);
-                                message_txt.focus();
-                            });
-
-                            $("#message_btn").click(function () {
-                                var text = $("#message_text").val();
-                                if (text.length <= 0)
-                                    return;
-                                message_txt.val("");
-                                socket.emit("message", {message: text, name: name});
-                            });
-
-                            function safe(str) {
-                                return str.replace(/&/g, '&amp;')
-                                    .replace(/</g, '&lt;')
-                                    .replace(/>/g, '&gt;');
-                            }
-
-                            $scope.isLog = true;
-                        }
-                        else {
-                            console.log(data);
-                            $scope.login_error = data;
-                        }
-                    });
-                };
-
-                $(document).ready(function () {
-
-
-
-
                 });
 
             });
         })
     });
+});
+
+app.controller("chatCtrl", function($scope, $rootScope, $http) {
+    $rootScope.isLog = false;
+    $scope.login_error = false;
+
+    $scope.login = function (login, password) {
+        $http({
+            url: "/chat/signin",
+            method: "POST",
+            data: {
+                login: login,
+                password: password
+            }
+        }).success(function (data) {
+            if (data._id) {
+
+                $rootScope.users_online = [];
+
+                $scope.login_error = false;
+
+                //socket.on('login', function (data) {
+                //    var message = "Welcome to Socket.IO Chat – ";
+                //    log(message, {
+                //        prepend: true
+                //    });
+                //    addParticipantsMessage(data);
+                //});
+                //
+                //var user_name = data.login;
+                //var socket = io();
+                //var name = data.login;
+                //var messages = $("#messages");
+                //var message_txt = $("#message_text");
+                //$('.chat .nick').text(name);
+                //
+                //function msg(nick, message) {
+                //    var m = '<div class="msg">' +
+                //        '<span class="user">' + safe(nick) + ':</span> '
+                //        + safe(message) +
+                //        '</div>';
+                //    messages
+                //        .append(m)
+                //        .scrollTop(messages[0].scrollHeight);
+                //}
+                //
+                //function msg_system(message) {
+                //    var m = '<div class="msg system">' + safe(message) + '</div>';
+                //    messages
+                //        .append(m)
+                //        .scrollTop(messages[0].scrollHeight);
+                //}
+                //
+                //socket.on('connecting', function () {
+                //    msg_system('Connecting...');
+                //});
+                //
+                //socket.on('connect', function (data) {
+                //    socket.emit('add user', user_name);
+                //    msg_system('Successfully connected to map chat!');
+                //    socket.emit("message", {message: name + ' connected!', name: 'Admin'});
+                //});
+                //
+                //socket.on('message', function (data) {
+                //    msg(data.name, data.message);
+                //    message_txt.focus();
+                //});
+                //
+                //$("#message_btn").click(function () {
+                //    var text = $("#message_text").val();
+                //    if (text.length <= 0)
+                //        return;
+                //    message_txt.val("");
+                //    socket.emit("message", {message: text, name: name});
+                //});
+                //
+                //function safe(str) {
+                //    return str.replace(/&/g, '&amp;')
+                //        .replace(/</g, '&lt;')
+                //        .replace(/>/g, '&gt;');
+                //}
+                var FADE_TIME = 150; // ms
+                var TYPING_TIMER_LENGTH = 400; // ms
+                var COLORS = [
+                    '#e21400', '#91580f', '#f8a700', '#f78b00',
+                    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+                    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+                ];
+
+                // Initialize varibles
+                var $window = $(window);
+                var $usernameInput = $('.usernameInput'); // Input for username
+                var $messages = $('.messages'); // Messages area
+                var $inputMessage = $('.inputMessage'); // Input message input box
+
+                var $loginPage = $('.login.page'); // The login page
+                var $chatPage = $('.chat.page'); // The chatroom page
+
+                // Prompt for setting a username
+                var username;
+                var connected = false;
+                var typing = false;
+                var lastTypingTime;
+                var $currentInput = $usernameInput.focus();
+
+                var socket = io();
+
+                function addParticipantsMessage (data) {
+                    var message = '';
+                    if (data.numUsers === 1) {
+                        message += "there's 1 participant";
+                    } else {
+                        message += "there are " + data.numUsers + " participants";
+                    }
+                    console.log($rootScope.users_online);
+                    $rootScope.users_online = $.map(data.listUsers, function(el) { return el });
+                    log(message);
+                }
+
+                function setUsername () {
+                    username = cleanInput($usernameInput.val().trim());
+
+                    // If the username is valid
+                    if (username) {
+                        $loginPage.fadeOut();
+                        $chatPage.show();
+                        $loginPage.off('click');
+                        $currentInput = $inputMessage.focus();
+
+                        // Tell the server your username
+                        socket.emit('add user', username);
+                    }
+                }
+
+                function sendMessage () {
+                    var message = $inputMessage.val();
+                    // Prevent markup from being injected into the message
+                    message = cleanInput(message);
+                    // if there is a non-empty message and a socket connection
+                    if (message && connected) {
+                        $inputMessage.val('');
+                        addChatMessage({
+                            username: username,
+                            message: message
+                        });
+                        // tell server to execute 'new message' and send along one parameter
+                        socket.emit('new message', message);
+                    }
+                }
+
+                function log (message, options) {
+                    var $el = $('<li>').addClass('log').text(message);
+                    addMessageElement($el, options);
+                }
+
+                function addChatMessage (data, options) {
+
+                    var $typingMessages = getTypingMessages(data);
+                    options = options || {};
+                    if ($typingMessages.length !== 0) {
+                        options.fade = false;
+                        $typingMessages.remove();
+                    }
+
+                    var $usernameDiv = $('<span class="username"/>')
+                        .text(data.username)
+                        .css('color', getUsernameColor(data.username));
+                    var $messageBodyDiv = $('<span class="messageBody">')
+                        .text(data.message);
+
+                    var typingClass = data.typing ? 'typing' : '';
+                    var $messageDiv = $('<li class="message"/>')
+                        .data('username', data.username)
+                        .addClass(typingClass)
+                        .append($usernameDiv, $messageBodyDiv);
+
+                    addMessageElement($messageDiv, options);
+                }
+
+                function addChatTyping (data) {
+                    data.typing = true;
+                    data.message = 'is typing';
+                    addChatMessage(data);
+                }
+
+                function removeChatTyping (data) {
+                    getTypingMessages(data).fadeOut(function () {
+                        $(this).remove();
+                    });
+                }
+
+                function addMessageElement (el, options) {
+                    var $el = $(el);
+
+                    if (!options) {
+                        options = {};
+                    }
+                    if (typeof options.fade === 'undefined') {
+                        options.fade = true;
+                    }
+                    if (typeof options.prepend === 'undefined') {
+                        options.prepend = false;
+                    }
+
+                    // Apply options
+                    if (options.fade) {
+                        $el.hide().fadeIn(FADE_TIME);
+                    }
+                    if (options.prepend) {
+                        $messages.prepend($el);
+                    } else {
+                        $messages.append($el);
+                    }
+                    $messages[0].scrollTop = $messages[0].scrollHeight;
+                }
+
+                function cleanInput (input) {
+                    return $('<div/>').text(input).text();
+                }
+
+                function updateTyping () {
+                    if (connected) {
+                        if (!typing) {
+                            typing = true;
+                            socket.emit('typing');
+                        }
+                        lastTypingTime = (new Date()).getTime();
+
+                        setTimeout(function () {
+                            var typingTimer = (new Date()).getTime();
+                            var timeDiff = typingTimer - lastTypingTime;
+                            if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+                                socket.emit('stop typing');
+                                typing = false;
+                            }
+                        }, TYPING_TIMER_LENGTH);
+                    }
+                }
+
+                function getTypingMessages (data) {
+                    return $('.typing.message').filter(function (i) {
+                        return $(this).data('username') === data.username;
+                    });
+                }
+
+                function getUsernameColor (username) {
+                    var hash = 7;
+                    for (var i = 0; i < username.length; i++) {
+                        hash = username.charCodeAt(i) + (hash << 5) - hash;
+                    }
+                    var index = Math.abs(hash % COLORS.length);
+                    return COLORS[index];
+                }
+
+                $window.keydown(function (event) {
+                    // Auto-focus the current input when a key is typed
+                    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+                        $currentInput.focus();
+                    }
+                    // When the client hits ENTER on their keyboard
+                    if (event.which === 13) {
+                        if (username) {
+                            sendMessage();
+                            socket.emit('stop typing');
+                            typing = false;
+                        }
+                        //else {
+                        //    setUsername();
+                        //}
+                    }
+                });
+
+                $inputMessage.on('input', function() {
+                    updateTyping();
+                });
+
+                $loginPage.click(function () {
+                    $currentInput.focus();
+                });
+
+                $inputMessage.click(function () {
+                    $inputMessage.focus();
+                });
+
+                socket.on('login', function (data) {
+                    connected = true;
+                    var message = "Welcome to Socket.IO Chat – ";
+                    log(message, {
+                        prepend: true
+                    });
+                    addParticipantsMessage(data);
+                });
+
+                socket.on('new message', function (data) {
+                    addChatMessage(data);
+                });
+
+                socket.on('user joined', function (data) {
+                    log(data.username + ' joined');
+                    addParticipantsMessage(data);
+                });
+
+                socket.on('user left', function (data) {
+                    log(data.username + ' left');
+                    addParticipantsMessage(data);
+                    removeChatTyping(data);
+                });
+
+                socket.on('typing', function (data) {
+                    addChatTyping(data);
+                });
+
+                socket.on('stop typing', function (data) {
+                    removeChatTyping(data);
+                });
+
+                username = cleanInput($usernameInput.val().trim());
+                if (username) {
+                    $loginPage.fadeOut();
+                    $chatPage.show();
+                    $loginPage.off('click');
+                    $currentInput = $inputMessage.focus();
+                    socket.emit('add user', username);
+                }
+
+
+                $rootScope.isLog = true;
+            }
+            else {
+                console.log(data);
+                $scope.login_error = data;
+            }
+        });
+    };
 });
--- server.js
+++ server.js
@@ -44,14 +44,102 @@ var server = app.listen(port || 901, function() {
 
 var io  = require('socket.io').listen(server);
 
-io.sockets.on('connection', function (client) {
-    client.on('message', function (message) {
-        try {
-            client.emit('message', message);
-            client.broadcast.emit('message', message);
-        } catch (e) {
-            console.log(e);
-            client.disconnect();
+var usernames = {};
+var numUsers = 0;
+//io.sockets.on('connection', function (client, socket) {
+//
+//    client.on('message', function (message) {
+//        try {
+//            client.emit('message', message);
+//            client.broadcast.emit('message', message);
+//        } catch (e) {
+//            console.log(e);
+//            client.disconnect();
+//        }
+//    });
+//});
+//io.on('connection', function (socket) {
+//    socket.on('add user', function (username) {
+//        // we store the username in the socket session for this client
+//        socket.username = username;
+//        // add the client's username to the global list
+//        usernames[username] = username;
+//        ++numUsers;
+//        addedUser = true;
+//        socket.emit('login', {
+//            numUsers: numUsers
+//        });
+//        // echo globally (all clients) that a person has connected
+//        socket.broadcast.emit('user joined', {
+//            username: socket.username,
+//            numUsers: numUsers
+//        });
+//    });
+//    socket.on('disconnect', function () {
+//        // remove the username from global usernames list
+//        if (addedUser) {
+//            delete usernames[socket.username];
+//            --numUsers;
+//
+//            // echo globally that this client has left
+//            socket.broadcast.emit('user left', {
+//                username: socket.username,
+//                numUsers: numUsers
+//            });
+//        }
+//    });
+//});
+
+var usernames = {};
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+    var addedUser = false;
+
+    socket.on('new message', function (data) {
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    socket.on('add user', function (username) {
+        socket.username = username;
+        usernames[username] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers,
+            listUsers : usernames
+        });
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            listUsers : usernames,
+            numUsers: numUsers
+        });
+    });
+
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('disconnect', function () {
+        if (addedUser) {
+            delete usernames[socket.username];
+            --numUsers;
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers,
+                listUsers : usernames
+            });
         }
     });
 });
